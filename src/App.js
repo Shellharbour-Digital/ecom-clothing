e58075dc31eae93dbf17bd00e8003dcf18ff9880
Shellharbour-Digital <hello@shellharbour.digital>
2021-03-18 16:44:25 +1100

    updating the app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const CollectionPage = (props) => {
-  return(
-    <div>
-      <h1>{props.match.params.shopId.toUpperCase()} PAGE</h1>
-    </div>
-  );
-}
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/:shopId' component={CollectionPage} />
+        <Route  path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );

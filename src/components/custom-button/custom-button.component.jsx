a1b13264742c70151b5c949a4d561758cabe5a88
Shellharbour-Digital <hello@shellharbour.digital>
2021-03-23 22:16:34 +1100

    added styled components to header and homepage components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
+import { CustomButtonContainer } from './custom-button.styles';
+
+{/*
+  use below when collection-item.styles has been converted to styled components and remove ref to styles.scss
+
+  const CustomButton = ({ children, ...props }) => (
+    <CustomButtonContainer {...props}>
+      {children}
+    </CustomButtonContainer>
+  );
+*/}
+
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
   <button
     className={`${inverted ? 'inverted' : '' } ${

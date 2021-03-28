1eef294f12cd69c0752368aff2583caeb9412aaa
Shellharbour-Digital <hello@shellharbour.digital>
2021-03-28 21:56:11 +1100

    converted collection components, custom button component, and header component to styled components as well as adding @media styles to be mobile friendly

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,25 +4,10 @@ import './custom-button.styles.scss';
 
 import { CustomButtonContainer } from './custom-button.styles';
 
-{/*
-  use below when collection-item.styles has been converted to styled components and remove ref to styles.scss
-
-  const CustomButton = ({ children, ...props }) => (
-    <CustomButtonContainer {...props}>
-      {children}
-    </CustomButtonContainer>
-  );
-*/}
-
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-  <button
-    className={`${inverted ? 'inverted' : '' } ${
-      isGoogleSignIn ? 'google-sign-in' : ''
-    } custom-button`}
-    {...otherProps}
-  >
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>
     {children}
-  </button>
+  </CustomButtonContainer>
 );
 
 export default CustomButton;
